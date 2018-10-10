20f8f41af9bc6eeeea0a53eb2f92ab5b552be13c
Allie <axs9711@rit.edu>
2018-10-10 17:08:55 -0400

    A03_LERP deliverable

--- A03 - LERP/AppClass.cpp
+++ A03 - LERP/AppClass.cpp
@@ -2,7 +2,7 @@
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Allie Sangalli - axs9711@rit.edu";
 	
 	//Set the position and target of the camera
 	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
@@ -23,6 +23,7 @@ void Application::InitVariables(void)
 		m_uOrbits = 7;
 
 	float fSize = 1.0f; //initial size of orbits
+	float fRadius = 0.95f; //initial radius of each orbit
 
 	//creating a color using the spectrum 
 	uint uColor = 650; //650 is Red
@@ -36,7 +37,21 @@ void Application::InitVariables(void)
 	{
 		vector3 v3Color = WaveLengthToRGB(uColor); //calculate color based on wavelength
 		m_shapeList.push_back(m_pMeshMngr->GenerateTorus(fSize, fSize - 0.1f, 3, i, v3Color)); //generate a custom torus and add it to the meshmanager
+		
+		std::vector<vector3> stopsList; //declaration of list of stop positions for circle to lerp 
+		float angle = (2.0f*PI) / i; //total angle between each endpoint of each orbit
+		float currAngle = 0; //current angle, initially 0
+
+		//add each endpoint to the stop positions list, then increases the current angle
+		for (int j = 0; j < i; j++)
+		{
+			stopsList.push_back(vector3(cos(currAngle)*fRadius, sin(currAngle)*fRadius, 0));
+			currAngle += angle;
+		}
+
+		m_totalStopsList.push_back(stopsList); //adds the list of stops for current orbit to the total stops list
 		fSize += 0.5f; //increment the size for the next orbit
+		fRadius += 0.5f; //increment the radius for the next orbit
 		uColor -= static_cast<uint>(decrements); //decrease the wavelength
 	}
 }
@@ -64,18 +79,35 @@ void Application::Display(void)
 	*/
 	//m4Offset = glm::rotate(IDENTITY_M4, 1.5708f, AXIS_Z);
 
+	static float percent = 0;
+	static int index = 0;
+
 	// draw a shapes
 	for (uint i = 0; i < m_uOrbits; ++i)
 	{
 		m_pMeshMngr->AddMeshToRenderList(m_shapeList[i], glm::rotate(m4Offset, 1.5708f, AXIS_X));
 
+		int size = m_totalStopsList[i].size(); //number of endpoints on the current orbit
+
+		vector3 startPoint = m_totalStopsList[i][index % size]; //start point on the current orbit
+		vector3 endPoint = m_totalStopsList[i][(index+1) % size]; //end point on the current orbit
+
 		//calculate the current position
-		vector3 v3CurrentPos = ZERO_V3;
+		vector3 v3CurrentPos = glm::lerp(startPoint, endPoint, percent);
 		matrix4 m4Model = glm::translate(m4Offset, v3CurrentPos);
 
 		//draw spheres
 		m_pMeshMngr->AddSphereToRenderList(m4Model * glm::scale(vector3(0.1)), C_WHITE);
 	}
+
+	//reset percent when at 100%, then increment the index
+	if (percent >= 1.0f)
+	{
+		percent = 0;
+		index++;
+	}
+
+	percent += 0.03f; //increment the percent every frame
 
 	//render list call
 	m_uRenderCallCount = m_pMeshMngr->Render();
